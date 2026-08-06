f1c1d7c5d18be9553e1f5095d470836c314d4365
pracheeta-swain <pracheeta.swain12@gmail.com>
2026-08-05 21:23:36 -0400

    add comma
    Fixed minor grammatical issues and added missing commas in the student profile.

--- profile.js
+++ profile.js
@@ -1,7 +1,7 @@
 const studentProfile = {
   name: "Pracheeta Swain",
   tagline: "DREAM-High Scholar | Computational Biology Portfolio",
-  bio: "I am a high school student from New Jersey. My scientific interests are to learn more about cell biology. I am excited for this program to learn more about how to turn real world data into models using R and Python. A few of my interests are karate, swimming, and baking!",
+  bio: "I am a high school student from New Jersey. My scientific interests are to learn more about cell biology. I am excited for this program to learn more about how to turn real-world data into models using R and Python. A few of my interests are karate, swimming, and baking!",
   photo: "profile.jpg",
   contact: "Email: pracheetaswain12@gmail.com",
   rpubs: [
@@ -9,12 +9,12 @@ const studentProfile = {
     { title: "Finding Patterns with Heatmaps in Python", url: "https://rpubs.com/Pracheeta-s/1448173" },
     { title: "TCGA Patient Data", url: "https://rpubs.com/Pracheeta-s/1448397" },
     { title: "Heat Maps with TCGA Patient Sample mRNA", url: "https://rpubs.com/Pracheeta-s/1448787" },
-    { title: "Breast Cancer Cell Lines and Differential Gene Expression", url: "https://rpubs.com/Pracheeta-s/1450113" }
-    { title: "Predictive Modeling for Cancer Prognosis", url: "https://rpubs.com/Pracheeta-s/1451294" },
+    { title: "Breast Cancer Cell Lines and Differential Gene Expression", url: "https://rpubs.com/Pracheeta-s/1450113" },
+    { title: "Predictive Modeling for Cancer Prognosis", url: "https://rpubs.com/Pracheeta-s/1451294" }
   ],
   finalProject: {
     title: "Leukemia Awareness Campaign",
-    description: "My final project is a health campaign promoted on an informational scientific poster about Leukemia awareness. It'll include common symptoms, what to do next, when to be concerned, and a QR code for guidance and and more information.",
+    description: "My final project is a health campaign promoted on an informational scientific poster about Leukemia awareness. It'll include common symptoms, what to do next, when to be concerned, and a QR code for guidance and more information.",
     url: ""
   },
   github: [
